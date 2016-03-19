b0f69d38b79165614be6f0920849305f2390828d
stephen <stephen.a.atwood@gmail.com>
2016-03-18 19:44:25 -0500

    Added Line struct, and DrawView with functionality to track touches and draw lines. (Chapter 17)

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Stephen Atwood on 3/18/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
